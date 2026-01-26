1a9ff61082359df3f69c75255067ee34b8630324
Shruthi2819 <shruthisrinivasan47@gmail.com>
2026-01-26 15:13:29 +0530

    Add files via upload

---
+++ Check.java
@@ -0,0 +1,12 @@
+public class Check{
+public static void main(String[] args){
+int x = 15;
+
+if (x > 10){
+    System.out.println("Greater than 10");}
+else if (x > 20){
+    System.out.println("Greater than 20");}
+else
+    {System.out.println("Else block");}
+}
+}
---
+++ switchcase.java
@@ -0,0 +1,14 @@
+public class switchcase{
+public static void main(String[] args) {
+int equalsides = 3;
+switch (equalsides){
+case 1:
+System.out.println("SCALENE TRIANGLE");
+break;
+case 2:
+System.out.println("ISOCELES TRIANGLE");
+break;
+case 3:
+System.out.println("EQUILATERAL TRIANGLE");
+break;
+}}}
