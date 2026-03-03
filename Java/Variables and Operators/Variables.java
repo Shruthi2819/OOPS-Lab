45b84214d92b5576f0ff73c978054c41ba2d397b
Shruthi2819 <shruthisrinivasan47@gmail.com>
2026-03-03 21:48:04 +0530

    Add files via upload

---
+++ Java/Variables and Operators/Variables.java
@@ -0,0 +1,12 @@
+public class Variables{
+   public static void main(String[] args) {
+     String a ="This is a string.";
+     int b = 5;
+     double c = 28.11;
+     boolean d = true;
+     System.out.println(a);
+     System.out.println(b);
+     System.out.println(c);
+     System.out.println(d);
+  }
+}
